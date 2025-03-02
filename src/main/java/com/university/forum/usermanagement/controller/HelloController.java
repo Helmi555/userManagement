8dfe8b7e14c9ca3a1eb2e764eb0ff73a1905efa5
Helmi BA <helmiabdelghani00@gmail.com>
2025-03-02 23:52:23 +0100

    first commit

---
+++ src/main/java/com/university/forum/usermanagement/controller/HelloController.java
@@ -0,0 +1,41 @@
+package com.university.forum.usermanagement.controller;
+
+
+import com.university.forum.usermanagement.service.MessageProducer;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+    private final MessageProducer messageProducer;
+    public HelloController(final MessageProducer messageProducer) {
+        this.messageProducer = messageProducer;
+    }
+
+
+    @Operation(summary = "Say Hello", description = "Returns a hello message.")
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(defaultValue = "Helmi") String name) {
+        return "Hello, " + name + "!";
+    }
+    @Operation(summary = "say hi", description = "ll")
+    @GetMapping("/hi")
+    public String hi(@RequestParam (defaultValue = "monji") String name) {
+        return "Hi " + name + "!";
+    }
+
+    @Operation(summary = "test the rabbitmq")
+    @GetMapping("/test/{message}")
+    public String testRabbitmq(@PathVariable String message) {
+        messageProducer.sendMessage(message);
+        return "message sent : "+message;
+    }
+
+    @Operation(summary = "test the rabbitmq2")
+    @GetMapping("/test2/{message}")
+    public String testRabbitmq2(@PathVariable String message) {
+        messageProducer.sendMessage(message);
+        return "message sent : "+message;
+    }
+}
---
+++ src/main/java/com/university/forum/usermanagement/service/MessageProducer.java
@@ -0,0 +1,23 @@
+package com.university.forum.usermanagement.service;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageProducer {
+    private final RabbitTemplate rabbitTemplate;
+    private final String queueName = "testQueue";
+
+    public MessageProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(String message) {
+        rabbitTemplate.convertAndSend(queueName, message);
+        System.out.println("✅ Sent: " + message);
+    }
+    public void sendMessage2(String message) {
+        rabbitTemplate.convertAndSend(queueName, message);
+        System.out.println("Sent 2 : "+message);
+    }
+}
